977a7e5b7fa38475ca8b7d84beda9550f99b34ad
VariniChiara <chiara.varini@studio.unibo.it>
2019-06-24 12:43:24 +0200

    set proj sprint1 at the same point of sprint0

--- it.unibo.ddrSystem1/ddrsys.pl
+++ it.unibo.ddrSystem1/ddrsys.pl
@@ -3,3 +3,4 @@
 %====================================================================================
 context(ctx, "localhost",  "TCP", "8090" ).
  qactor( console, ctx, "it.unibo.console.Console").
+  qactor( robot, ctx, "it.unibo.robot.Robot").
