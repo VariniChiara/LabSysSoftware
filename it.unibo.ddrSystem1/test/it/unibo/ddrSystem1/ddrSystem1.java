36e10a504d5e5af2c428dfa15b8b854ed137bf90
VariniChiara <chiara.varini@studio.unibo.it>
2019-06-27 18:06:37 +0200

    add test skeleton

---
+++ it.unibo.ddrSystem1/test/it/unibo/ddrSystem1/ddrSystem1.java
@@ -0,0 +1,34 @@
+package it.unibo.ddrSystem1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ddrSystem1 {
+
+	@Test
+	public void initialPositionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void movingTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void wallDetectingTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void exploringAllTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void mapOkTest() {
+		fail("Not yet implemented");
+	}
+
+}
