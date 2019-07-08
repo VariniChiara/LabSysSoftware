d2fbc4df793f8a5adf7b0e7ed78ec7b635887646
federicapecci <federica.pecci2@studio.unibo.it>
2019-07-08 15:31:06 +0200

    add virtual robot exploring pos 1,1 and going back home

--- it.unibo.ddrSystem2/src/it/unibo/robotmind/Robotmind.kt
+++ it.unibo.ddrSystem2/src/it/unibo/robotmind/Robotmind.kt
@@ -15,57 +15,64 @@ class Robotmind ( name: String, scope: CoroutineScope ) : ActorBasicFsm( name, s
 	}
 		
 	override fun getBody() : (ActorBasicFsm.() -> Unit){
-		var obstacle = false
+		var obstacle = false 
+		var Curmove     = ""  
+		var IterCounter = 0
 		return { //this:ActionBasciFsm
 				state("s0") { //this:State
 					action { //it:State
 						solve("consult('ddrsys.pl')","") //set resVar	
 						solve("consult('resourceModel.pl')","") //set resVar	
-						println("Robot intialized")
+						println("Robot mind intialized")
 					}
-					 transition( edgeName="goto",targetState="waitForEvents", cond=doswitch() )
+					 transition( edgeName="goto",targetState="waitForStart", cond=doswitch() )
 				}	 
-				state("waitForEvents") { //this:State
+				state("waitForStart") { //this:State
 					action { //it:State
 					}
-					 transition(edgeName="t00",targetState="handleCmd",cond=whenDispatch("userCmd"))
-					transition(edgeName="t01",targetState="handleCmd",cond=whenDispatch("robotCmd"))
-					transition(edgeName="t02",targetState="handleSonarRobot",cond=whenEvent("sonarRobot"))
+					 transition(edgeName="t00",targetState="initMap",cond=whenDispatch("startCmd"))
 				}	 
-				state("handleCmd") { //this:State
+				state("initMap") { //this:State
 					action { //it:State
-						println("$name in ${currentState.stateName} | $currentMsg")
-						if( checkMsgContent( Term.createTerm("userCmd(CMD)"), Term.createTerm("userCmd(CMD)"), 
-						                        currentMsg.msgContent()) ) { //set msgArgList
-								solve("action(robot,move(${payloadArg(0)}))","") //set resVar	
-						}
-						if( checkMsgContent( Term.createTerm("robotCmd(CMD)"), Term.createTerm("robotCmd(CMD)"), 
-						                        currentMsg.msgContent()) ) { //set msgArgList
-								solve("action(robot,move(${payloadArg(0)}))","") //set resVar	
-						}
+						println("&&&  robotmind STARTED")
+						itunibo.planner.plannerUtil.initAI(  )
+						println("INITIAL MAP")
+						itunibo.planner.plannerUtil.showMap(  )
+						itunibo.planner.plannerUtil.setGoal( "1", "1"  )
+						itunibo.planner.moveUtils.doPlan(myself)
 					}
-					 transition( edgeName="goto",targetState="waitForEvents", cond=doswitch() )
+					 transition( edgeName="goto",targetState="doPlan", cond=doswitch() )
 				}	 
-				state("handleSonarRobot") { //this:State
+				state("doPlan") { //this:State
 					action { //it:State
-						println("$name in ${currentState.stateName} | $currentMsg")
-						if( checkMsgContent( Term.createTerm("sonarRobot(DISTANCE)"), Term.createTerm("sonarRobot(DISTANCE)"), 
-						                        currentMsg.msgContent()) ) { //set msgArgList
-								obstacle = Integer.parseInt( payloadArg(0) ) < 10 
-						}
+						solve("retract(move(M))","") //set resVar	
+						if(currentSolution.isSuccess()) { Curmove = getCurSol("M").toString()
+						itunibo.planner.moveUtils.doPlannedMove(myself ,Curmove )
+						forward("robotCmd", "robotCmd($Curmove)" ,"robotactuator" ) 
+						delay(1000) 
+						 }
+						else
+						{ Curmove="nomove" 
+						forward("robotCmd", "robotCmd(h)" ,"robotactuator" ) 
+						 }
 					}
-					 transition( edgeName="goto",targetState="handeObstacle", cond=doswitchGuarded({obstacle}) )
-					transition( edgeName="goto",targetState="waitForEvents", cond=doswitchGuarded({! obstacle}) )
+					 transition( edgeName="goto",targetState="doPlan", cond=doswitchGuarded({(Curmove != "nomove")}) )
+					transition( edgeName="goto",targetState="endOfJob", cond=doswitchGuarded({! (Curmove != "nomove")}) )
 				}	 
-				state("handeObstacle") { //this:State
+				state("endOfJob") { //this:State
 					action { //it:State
-						println("handleObstacle: going backward")
-						forward("robotCmd", "robotCmd(s)" ,"robotmind" ) 
-						delay(300) 
-						println("handeObstacle: stopping")
-						forward("robotCmd", "robotCmd(h)" ,"robotmind" ) 
+						println("FINAL MAP")
+						itunibo.planner.plannerUtil.showMap(  )
+						println("&&&  planex0 ENDS")
 					}
-					 transition( edgeName="goto",targetState="waitForEvents", cond=doswitch() )
+					 transition( edgeName="goto",targetState="goBackHome", cond=doswitch() )
+				}	 
+				state("goBackHome") { //this:State
+					action { //it:State
+						itunibo.planner.plannerUtil.setGoal( "0", "0"  )
+						itunibo.planner.moveUtils.doPlan(myself)
+					}
+					 transition( edgeName="goto",targetState="doPlan", cond=doswitch() )
 				}	 
 			}
 		}
