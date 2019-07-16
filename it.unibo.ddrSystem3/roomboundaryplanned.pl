8026f68a1ed295da2724a0e1094827e242429e93
VariniChiara <chiara.varini@studio.unibo.it>
2019-07-16 11:08:40 +0200

    add new ddrSystem3

--- it.unibo.ddrSystem3/roomboundaryplanned.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% roomboundaryplanned description   
-%====================================================================================
-mqttBroker("localhost", "1883").
-context(ctxboundaryplanned, "localhost",  "MQTT", "0" ).
-context(ctxdummyformind, "otherresourcelocalhost",  "MQTT", "0" ).
- qactor( resourcemodel, ctxdummyformind, "external").
-  qactor( onestepahead, ctxdummyformind, "external").
-  qactor( roomboudaryexplorer, ctxboundaryplanned, "it.unibo.roomboudaryexplorer.Roomboudaryexplorer").
