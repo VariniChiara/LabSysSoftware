d7b03d18de1ef7d51719e96619df80783d8cc60e
VariniChiara <chiara.varini@studio.unibo.it>
2019-08-31 11:16:59 +0200

    create new project ddrSystem6

---
+++ it.unibo.ddrSystem6/exploration.pl
@@ -0,0 +1,12 @@
+%====================================================================================
+% exploration description   
+%====================================================================================
+mqttBroker("localhost", "1883").
+context(robotmindctx, "localhost",  "MQTT", "0" ).
+context(robotresourcectx, "192.168.1.164",  "MQTT", "0" ).
+ qactor( planexecutor, robotmindctx, "it.unibo.planexecutor.Planexecutor").
+  qactor( robotmind, robotmindctx, "it.unibo.robotmind.Robotmind").
+  qactor( onestepahead, robotmindctx, "it.unibo.onestepahead.Onestepahead").
+  qactor( robotactuator, robotresourcectx, "it.unibo.robotactuator.Robotactuator").
+  qactor( sonarhandler, robotmindctx, "it.unibo.sonarhandler.Sonarhandler").
+  qactor( resourcemodel, robotmindctx, "it.unibo.resourcemodel.Resourcemodel").
